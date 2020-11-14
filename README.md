6b55c064e77dde090cb20a6ffa3f8977309cba23
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-11-14 15:05:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CProject
+
+Developed with Unreal Engine 4
+By Matilde Rodrigues
---
+++ Source/CProject/CProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACProjectGameMode();
+};
+
+
+
